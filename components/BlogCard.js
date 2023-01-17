2fbc9e04d3c3d943687b49f0ee7c7b03e9277291
Nick Boniatis <94140534+NickBon7@users.noreply.github.com>
2023-01-17 17:37:53 +0000

    Update BlogCard.js

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -7,7 +7,7 @@ const BlogPost = ({ title, author, coverPhoto, datePublished, slug }) => {
     <div className={styles.card}>
       <Link href={"/posts/" + slug}>
         <div className={styles.imgContainer}>
-          <Image src={coverPhoto.url} alt="" width={600} height={115} />
+          <Image src={coverPhoto.url} alt="" width={600} height={145} />
         </div>
       </Link>
       <div className={styles.text}>
